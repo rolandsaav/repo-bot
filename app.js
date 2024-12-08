2d6c789b56958e62b06ee036109e894320790c10
Roland Saavedra <rolandsaavedra22@gmail.com>
2024-12-08 17:07:52 -0500

    Added Login with github route and setup callback

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import { App } from "octokit"
 import { createNodeMiddleware } from "@octokit/webhooks"
 import { createNodeMiddleware as createOtherMiddleware } from "@octokit/app"
 import fs from "fs"
-import http from "http"
 import { client } from "./twilio.js"
 import express from "express"
 
@@ -13,6 +12,8 @@ const appId = process.env.APP_ID;
 const webhookSecret = process.env.WEBHOOK_SECRET;
 const privateKeyPath = process.env.PRIVATE_KEY_PATH;
 const verificationSecret = process.env.TWILIO_VERIFY_SID;
+const clientSecret = process.env.CLIENT_SECRET
+const clientId = process.env.CLIENT_ID
 
 const privateKey = fs.readFileSync(privateKeyPath, "utf8")
 
@@ -30,6 +31,7 @@ async function verifyNumber(phoneNumber) {
         .verifications
         .create({ to: phoneNumber, channel: "sms" })
     console.log(verification.sid)
+    return verification
 }
 
 async function handlePush({ octokit, payload }) {
@@ -56,9 +58,7 @@ app.webhooks.onError((error) => {
 });
 
 const port = 3000;
-const host = "localhost"
 const path = "/api/webhook";
-const localWebhookUrl = `http://${host}:${port}${path}`
 
 const webhookMiddleware = createNodeMiddleware(app.webhooks, { path });
 
@@ -68,12 +68,43 @@ expressApp.use(webhookMiddleware);
 
 expressApp.use(express.json())
 
-expressApp.post("/verifyNumber", (req, res) => {
+expressApp.post("/verifyNumber", async (req, res) => {
     const phoneNumber = req.body.phone
-
     console.log(phoneNumber)
+    const verification = await client.verify.v2.services(verificationSecret)
+        .verifications
+        .create({ to: phoneNumber, channel: "sms" })
+
+    console.log(verification.status)
 
     res.send(`Verify Number: ${phoneNumber}`)
+})
+
+expressApp.post("/checkcode", async (req, res) => {
+    const phoneNumber = req.body.phone
+    const code = req.body.code
+
+    const verificationCheck = await client.verify.v2.services(verificationSecret)
+        .verificationChecks
+        .create({ to: phoneNumber, code: code })
+
+    const status = verificationCheck.status
+
+    if (status != "approved") {
+        res.send("Approval failed")
+    } else {
+        res.send("Approved")
+    }
+})
+
+expressApp.get("/", (req, res) => {
+    res.send(`<a href="https://www.github.com/login/oauth/authorize?client_id=${clientId}">Login with Github</a>`)
+})
+
+expressApp.get("/github/callback", (req, res) => {
+    const code = req.query.code
+
+    res.send(`Successfully authorized! Got code ${code}`)
 })
 
 expressApp.listen(port, () => {
